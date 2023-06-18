92acc00af01b477fcbdc5a3295e7ed7f79563864
Pola Eskandar <polaeskandar123@gmail.com>
2023-06-19 00:14:38 +0200

    Fixed authorization issue.

--- api/app/Providers/AuthServiceProvider.php
+++ api/app/Providers/AuthServiceProvider.php
@@ -2,25 +2,29 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\Post;
+use App\Policies\PostPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
+  /**
+   * The model to policy mappings for the application.
+   *
+   * @var array<class-string, class-string>
+   */
+  protected $policies = [
+//    Post::class => PostPolicy::class
+  ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+  /**
+   * Register any authentication / authorization services.
+   */
+  public function boot(): void
+  {
+//    $this->registerPolicies();
+//    Gate::resource('posts', PostPolicy::class);
+  }
 }
